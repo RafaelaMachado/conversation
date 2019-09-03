07e53c4e9d52612b930a0c37efab9fa44ae85251
Rafaela Machado <mmrafaela@localhost.localdomain>
2019-09-03 19:25:08 -0300

    #13 - functional sigin form

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -10,15 +10,18 @@
                         <form>
                             <div class="form-group">
                                 <label for="email">Email address</label>
-                                <input type="email" id="email" class="form-control" placeholder="Email address">
+                                <input type="email" id="email" class="form-control" placeholder="Email address" v-model.trim="email">
                             </div>
                             <div class="form-group">
                                 <label for="password">Password</label>
-                                <input type="password" id="password" class="form-control" placeholder="Password">
+                                <input type="password" id="password" class="form-control" placeholder="Password" v-model.trim="password" @keyup.enter="signIn">
                             </div>
-                            <button type="button" class="btn btn-purple btn-block" >Sign In</button>
+                            <button type="button" class="btn btn-purple btn-block" :disabled="isLoading" @click="signIn">Sign In</button>
                         </form>
                     </div>
+                </div>
+                <div class="mt-2 alert alert-danger" v-if="errors.length">
+                    <div v-for="(error, index) in errors" :key="`error-${index}`"> {{error}}</div>
                 </div>
                 <div class="mt-2 text-center create-account-callout">
                     New to Conversations? <router-link to="/signUp">Create an account.</router-link>
@@ -30,6 +33,38 @@
 
 <script>
 export default {
-    name: 'SignIn'
+    name: 'SignIn',
+    data () {
+        return {
+            email: '',
+            password: '',
+            errors: [],
+            isLoading: false
+        }
+    },
+    methods: {
+        signIn () {
+            this.errors = []
+            if (this.isLoading || !this.isFormValid()) return
+            this.isLoading = true
+            window.firebase.auth()
+                .signInWithEmailAndPassword(this.email, this.password)
+                .then(user => {
+                    this.$store.dispatch('setCurrentUser', user)
+                    this.$router.push('/')
+                })
+                .catch(error => {
+                    this.errors.push(error.message)
+                    this.isLoading = false
+                })
+        },
+        isFormValid () {
+            if (!this.email.length || !this.password.length) {
+                this.errors.push('All fields are required')
+                return false
+            }
+            return true
+        }
+    }
 }
 </script>
