8260c3279b78e79a577afc245a23056dc8ac8076
Rafaela Machado <mmrafaela@localhost.localdomain>
2019-09-03 13:31:42 -0300

    #4 - Created a SignIn static layout

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,72 @@
         <router-view/>
     </div>
 </template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Open+Sans|Satisfy|Source+Code+Pro&display=swap');
+
+html, body, #app {
+    height: 100%;
+}
+
+* {
+    font-family: 'Open Sans', sans-serif;
+}
+
+pre {
+    font-family: 'Source Code Pro', monospace !important;
+}
+
+$purple: #6642a8;
+
+.form-control:focus {
+    border-color: $purple !important;
+    box-shadow: 0 0 0 0.2rem rgba($purple, .25) !important;
+}
+
+.btn.focus, .btn:focus {
+    box-shadow: 0 0 0 0.2rem rgba($purple, .25) !important;
+}
+
+.text-30 {
+    font-size: 30px;
+}
+
+.btn-link {
+    color: $purple !important;
+}
+
+.btn-purple {
+    background-color: $purple !important;
+    color: #fff !important;
+}
+
+.create-account-callout {
+    border: 1px solid #d8dee2;
+    border-radius: 5px;
+    padding: 15px 20px;
+    text-align: center;
+}
+
+::-webkit-scrollbar {
+    width: 12px;
+}
+
+::-webkit-scrollbar:horizontal {
+    height: 12px;
+}
+
+::-webkit-scrollbar-track {
+    box-shadow: none;
+    background-color: rgba(#f5f5f5, .5);
+    border-radius: 12px;
+}
+
+::-webkit-scrollbar-thumb {
+    border-radius: 12px;
+    background-color: rgba(#ccc, .5);
+    &:hover {
+        background-color: rgba(#ccc, .9);
+    }
+}
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,6 +1,30 @@
 <template>
-    <div>
-        SignIn
+    <div class="d-flex flex-column justify-content-center align-items-center h-100 bg-light">
+        <div class="row w-100">
+            <div class="col col-sm-12 col-md-4 offset-md-4">
+                <div class="col text-center text-30 pb-2">
+                    Sign in to Conversations
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" id="email" class="form-control" placeholder="Email address">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" id="password" class="form-control" placeholder="Password">
+                            </div>
+                            <button type="button" class="btn btn-purple btn-block" >Sign In</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="mt-2 text-center create-account-callout">
+                    New to Conversations? <router-link to="/signUp">Create an account.</router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,6 +33,3 @@ export default {
     name: 'SignIn'
 }
 </script>
-
-<style>
-</style>
